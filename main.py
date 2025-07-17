54588360f3658a70f56cbfa8e04814b379074f28
Krzysztof Arciszuk <karciszuk@gmail.com>
2025-07-17 13:28:28 +0200

    queries improvements

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sql_execution import SQL_Executor
 from queries import SQL_Queries
+import pandas as pd
 
 db_path = 'D:/VB/viabill.db'
 sql_e = SQL_Executor(db_path)
@@ -7,10 +8,11 @@ sql_e = SQL_Executor(db_path)
 def main(query):
     try:
         results = sql_e.execute_query(query)
-        for row in results:
-            print(row)
+        results_df = pd.DataFrame(results, columns=[col[0] for col in sql_e.cur.description])
+        print(results_df)
     finally:
         sql_e.cleanup()
 
 if __name__ == "__main__":
-    main(SQL_Queries.monthly_users)
+    query = SQL_Queries.Portfolio.new_customers()
+    main(query)
--- queries.py
+++ queries.py
@@ -1,4 +1,57 @@
 class SQL_Queries:
-    test =  "SELECT * FROM Users LIMIT 10;"
+    
+    class Portfolio:
+        @staticmethod
+        def new_customers():
+            return """
+            SELECT COUNT(DISTINCT user_id) AS user_count, strftime('%Y-%m', registration_date) AS month 
+            FROM Users 
+            WHERE registration_date > (
+                SELECT date(MAX(registration_date), '-12 months') 
+                FROM Users
+            )
+            GROUP BY month 
+            ORDER BY month
+            """
+        
+        @staticmethod
+        def active_customers():
+            return 
+        
+        @staticmethod
+        def transaction_volume():
+            return
+        
+        @staticmethod
+        def installment_plans():
+            return
+        
+        @staticmethod
+        def popular_categories():
+            return
+    
+    class Payment:
+        @staticmethod
+        def define_dpd_aggregate():
+            return
+        
+        @staticmethod
+        def define_dpd90_rate():
+            return
+        
+        @staticmethod
+        def calculate_dpd90_trend():
+            return
+    
+    class Vintage:
+        @staticmethod
+        def group_by_first_transaction():
+            return
 
-    monthly_users = "SELECT COUNT(user_id) AS user_count, strftime('%m', registration_date) AS month FROM Users GROUP BY month ORDER BY month"
+        @staticmethod
+        def dpd90_status_after_months():
+            return
+        
+        @staticmethod
+        def dpd90_percentage_per_month():
+            return
