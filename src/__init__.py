15e6cd58e43e4a11bebd6cfb946b5341aaeb5600
Krzysztof Arciszuk <karciszuk@gmail.com>
2025-07-23 20:09:11 +0200

    setup and notebook upgrades

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,8 @@
 from .sql_execution import SQL_Executor
 from .queries import SQL_Queries
-from .utils import group_by_column, create_labels, dpd_calc, smart_format
+
 
 __all__ = [
     "SQL_Executor",
     "SQL_Queries",
-    "group_by_column",
-    "create_labels",
-    "dpd_calc",
-    "smart_format",
 ]
